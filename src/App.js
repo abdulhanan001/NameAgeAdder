780b1d7cc59ce7b8666924f5c5a8b7a890ff6faa
Abdul Hanan <abdulhanan347@gmail.com>
2022-12-18 13:38:19 +0500

    COMPONENT::USERSLIST::Added Users Lists component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import {React, useState} from 'react';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+    });
+  }
+
   return (
-    <AddUser />
+    <>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </>
   );
 }
 
